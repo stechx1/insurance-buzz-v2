5880f319c59f1cf41aa0a9f4a4b813933fd21c25
techx1 <ss.uitech01@gmail.com>
2023-09-16 19:37:20 +0500

    added contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,12 +1,23 @@
 import { ContactHero } from '@/components/ContactHero/ContactHero';
 import { ContactPageForm } from '@/components/ContactPageForm/ContactPageForm';
+import Iframe from 'react-iframe';
+
 
 const Contact = () => {
   return (
     <div className=''>
       <ContactHero />
       <ContactPageForm />
-      <img src="/images/map.png" alt="map" />
+      <div style={{ width: '100%', height: '400px' }}>
+        <Iframe
+          url={`https://www.google.com/maps/embed/v1/place?key=&q=925+Lawson+Road+%2340,+London,+ON+N6G+4X3`}
+          width='100%'
+          height='100%'
+          frameBorder='0'
+          style={{ border: '0' }}
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 };
