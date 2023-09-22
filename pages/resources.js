f83995709501f5628672becc228488620953d8cc
techx1 <ss.uitech01@gmail.com>
2023-09-22 11:31:26 +0500

    added links + resources

--- pages/resources.js
+++ pages/resources.js
@@ -1,81 +1,116 @@
-import { Anchor, Collapse } from "antd";
-import { lifeInsuranceItems } from "@/data/faqs/life-insurance";
-import { travelInsuranceItems } from "@/data/faqs/travel-insurance";
-import { termLifeInsuranceItems } from "@/data/faqs/term-life-insurance";
-import { disabilityInsuranceItems } from "@/data/faqs/disability-insurance";
-import { claimInsuranceItems } from "@/data/faqs/claim-insurance";
-import { criticalIllnessItems } from "@/data/faqs/critical-illness-insurance";
-import Link from "next/link";
-import { healthInsuranceItems } from "@/data/faqs/health-insurance";
-import { respInsuranceItems } from "@/data/faqs/resp";
-import { tfsaInsuranceItems } from "@/data/faqs/tfsa";
-import { rrspInsuranceItems } from "@/data/faqs/rrsp";
-import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { Anchor, Collapse } from 'antd';
+import { lifeInsuranceItems } from '@/data/faqs/life-insurance';
+import { travelInsuranceItems } from '@/data/faqs/travel-insurance';
+import { termLifeInsuranceItems } from '@/data/faqs/term-life-insurance';
+import { disabilityInsuranceItems } from '@/data/faqs/disability-insurance';
+import { claimInsuranceItems } from '@/data/faqs/claim-insurance';
+import { criticalIllnessItems } from '@/data/faqs/critical-illness-insurance';
+import Link from 'next/link';
+import { healthInsuranceItems } from '@/data/faqs/health-insurance';
+import { respInsuranceItems } from '@/data/faqs/resp';
+import { tfsaInsuranceItems } from '@/data/faqs/tfsa';
+import { rrspInsuranceItems } from '@/data/faqs/rrsp';
+import { HeroSection } from '@/components/HeroSection/HeroSection';
+import { superVisaInsuranceFAQs } from '@/data/faqs/super-visa-insurance';
+import { internationalStudentsInsuranceFAQs } from '@/data/faqs/international-student-insurance';
 
 const ResourcesPage = () => {
   return (
     <>
       <HeroSection
-        heading={
-          "Frequently Asked Questions"
-        }
+        heading={'Frequently Asked Questions'}
         para={`Welcome to the hub of frequently asked questions on insurance.`}
-        img={"/images/sub/FAQ-hero-image.jpeg"}
-        color={"#5393becc"}
-        position="top"
+        img={'/images/sub/FAQ-hero-image.jpeg'}
+        color={'#5393becc'}
+        position='top'
       />
-      <div className="bg-[#305fa9] relative top-[-165px]">
-        <div className="grid grid-cols-6">
+      <div className='bg-[#305fa9] relative top-[-165px]'>
+        <div className='grid grid-cols-6'>
           <Anchor
             offsetTop={150}
-            style={{ background: "", height: "100vh", paddingTop: "40px" }}
+            style={{ background: '', height: '100vh', paddingTop: '40px' }}
             replace
             items={[
               {
-                key: "life-insurance",
-                href: "#life-insurance",
-                title: "Life Insurance",
+                key: 'life-insurance',
+                href: '#life-insurance',
+                title: 'Life Insurance',
               },
               {
-                key: "travel-insurance",
-                href: "#travel-insurance",
-                title: "Travel Insurance",
+                key: 'health-insurance',
+                href: '#health-insurance',
+                title: 'Health Insurance',
               },
               {
-                key: "Term Insurance",
-                href: "#term-insurance",
-                title: "Term Insurance",
+                key: 'term-insurance',
+                href: '#term-insurance',
+                title: 'Term Life Insurance',
               },
               {
-                key: "disability-insurance",
-                href: "#disability-insurance",
-                title: "Disability Insurance",
+                key: 'travel-insurance',
+                href: '#travel-insurance',
+                title: 'Travel Insurance',
               },
               {
-                key: "critical-insurance",
-                href: "#critical-insurance",
-                title: "Critical Illness Insurance",
+                key: 'supervisa-insurance',
+                href: '#supervisa-insurance',
+                title: 'Super Visa Insurance',
               },
               {
-                key: "claim-insurance",
-                href: "#claim-insurance",
-                title: "Claim Process",
+                key: 'international-students-insurance',
+                href: '#international-insurance',
+                title: 'International Students Insurance',
               },
               {
-                key: "important-links",
-                href: "#important-links",
-                title: "Important Links",
+                key: 'disability-insurance',
+                href: '#disability-insurance',
+                title: 'Disability Insurance',
+              },
+              {
+                key: 'critical-insurance',
+                href: '#critical-insurance',
+                title: 'Critical Illness Insurance',
+              },
+              {
+                key: 'resp',
+                href: '#resp',
+                title: 'RESP',
+              },
+              {
+                key: 'rrsp',
+                href: '#rrsp',
+                title: 'RRSP',
+              },
+              {
+                key: 'tfsa',
+                href: '#tfsa',
+                title: 'TFSA',
+              },
+              {
+                key: 'critical-insurance',
+                href: '#critical-insurance',
+                title: 'Critical Illness Insurance',
+              },
+              {
+                key: 'claim-insurance',
+                href: '#claim-insurance',
+                title: 'Claim Process',
+              },
+              {
+                key: 'important-links',
+                href: '#important-links',
+                title: 'Important Links',
               },
             ]}
           />
 
-          <div className="col-span-5 pt-[20px] container mr-20">
+          <div className='col-span-5 pt-[20px] container mr-20'>
             <div
-              className="p-4 "
-              id="life-insurance"
-              style={{ minHeight: "100vh", background: "#305fa9" }}
+              className='p-4 '
+              id='life-insurance'
+              style={{ minHeight: '100vh', background: '#305fa9' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 Life Insurance FAQs
               </h1>
               <Collapse
@@ -85,11 +120,11 @@ const ResourcesPage = () => {
               />
             </div>
             <div
-              className="p-4 "
-              id="life-insurance"
-              style={{ minHeight: "100vh", background: "#61b1bc" }}
+              className='p-4 '
+              id='health-insurance'
+              style={{ minHeight: '100vh', background: '#61b1bc' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 Health Insurance FAQs
               </h1>
               <Collapse
@@ -99,11 +134,11 @@ const ResourcesPage = () => {
               />
             </div>
             <div
-              className="p-4"
-              id="travel-insurance"
-              style={{ minHeight: "100vh", background: "#9ab88c" }}
+              className='p-4'
+              id='travel-insurance'
+              style={{ minHeight: '100vh', background: '#9ab88c' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 Travel Insurance FAQs
               </h1>
               <Collapse
@@ -113,11 +148,11 @@ const ResourcesPage = () => {
               />
             </div>
             <div
-              id="term-insurance"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#ed8b08" }}
+              id='term-insurance'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#ed8b08' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 Term Insurance FAQs
               </h1>
               <Collapse
@@ -127,11 +162,35 @@ const ResourcesPage = () => {
               />
             </div>
             <div
-              id="disability-insurance"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#6f909e" }}
+              className='p-4'
+              id='supervisa-insurance'
+              style={{ minHeight: '100vh', background: '#d6b940' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
+                Super Visa Insurance FAQs
+              </h1>
+              <Collapse
+                ghost
+                items={superVisaInsuranceFAQs}
+                // defaultActiveKey={['1']}
+              />
+            </div>
+            <div
+              className='p-4'
+              id='international-students-insurance'
+              style={{ minHeight: '100vh', background: '#6f909e' }}
+            >
+              <h1 className='text-white font-bold text-2xl'>
+                International Student Insurance FAQs
+              </h1>
+              <Collapse ghost items={internationalStudentsInsuranceFAQs} />
+            </div>
+            <div
+              id='disability-insurance'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#6f909e' }}
+            >
+              <h1 className='text-white font-bold text-2xl'>
                 Disability Insurance FAQs
               </h1>
               <Collapse
@@ -141,11 +200,11 @@ const ResourcesPage = () => {
               />
             </div>
             <div
-              id="critical-insurance"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#308b84" }}
+              id='critical-insurance'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#308b84' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 Critical Illness Insurance FAQs
               </h1>
               <Collapse
@@ -154,26 +213,13 @@ const ResourcesPage = () => {
                 // defaultActiveKey={['1']}
               />
             </div>
+
             <div
-              id="claim-insurance"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#a94342" }}
+              id='resp'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#5393becc' }}
             >
-              <h1 className="text-white font-bold text-2xl">
-                Claim Process FAQs
-              </h1>
-              <Collapse
-                ghost
-                items={claimInsuranceItems}
-                // defaultActiveKey={['1']}
-              />
-            </div>
-            <div
-              id="resp"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#5393becc" }}
-            >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 RESP Insurance FAQs
               </h1>
               <Collapse
@@ -184,11 +230,11 @@ const ResourcesPage = () => {
             </div>
 
             <div
-              id="tfsa"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#9AB88CCC" }}
+              id='tfsa'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#9AB88CCC' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 TFSA Insurance FAQs
               </h1>
               <Collapse
@@ -199,11 +245,11 @@ const ResourcesPage = () => {
             </div>
 
             <div
-              id="rrsp"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#256c18CC" }}
+              id='rrsp'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#256c18CC' }}
             >
-              <h1 className="text-white font-bold text-2xl">
+              <h1 className='text-white font-bold text-2xl'>
                 RRSP Insurance FAQs
               </h1>
               <Collapse
@@ -214,64 +260,72 @@ const ResourcesPage = () => {
             </div>
 
             <div
-              id="important-links"
-              className="p-4"
-              style={{ minHeight: "100vh", background: "#237a23" }}
+              id='important-links'
+              className='p-4'
+              style={{ minHeight: '100vh', background: '#237a23' }}
             >
-              <div className=" container mx-auto">
-                <h2 className="text-3xl font-bold text-white">
+              <div className=' container mx-auto'>
+                <h2 className='text-3xl font-bold text-white'>
                   Important Links
                 </h2>
               </div>
 
-              <div className="mt-8 container mx-auto flex flex-col gap-4">
+              <div className='mt-8 container mx-auto flex flex-col gap-4'>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html"
+                    'https://www.canada.ca/en/revenue-agency/services/tax/international-non-residents/individuals-leaving-entering-canada-non-residents/newcomers-canada-immigrants.html'
                   }
                 >
-                  https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html
+                  Newcomers to Canada
                 </Link>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/immigration-refugees-citizenship/services/application/application-forms-guides/guide-5256-applying-visitor-visa-temporary-resident-visa.html#incometables"
+                    'https://www.canada.ca/en/immigration-refugees-citizenship/services/new-immigrants.html'
                   }
                 >
-                  https://www.canada.ca/en/immigration-refugees-citizenship/services/application/application-forms-guides/guide-5256-applying-visitor-visa-temporary-resident-visa.html#incometables
+                  Services and information for new Immigrants{' '}
                 </Link>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/parent-grandparent-super-visa/eligibility.html"
+                    'https://www.ontario.ca/page/apply-ohip-and-get-health-card'
                   }
                 >
-                  https://www.canada.ca/en/immigration-refugees-citizenship/services/visit-canada/parent-grandparent-super-visa/eligibility.html
+                  Apply for OHIP and get a health card
                 </Link>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html"
+                    'https://www.canada.ca/en/immigration-refugees-citizenship/services/application/medical-police/medical-exams.html'
                   }
                 >
-                  https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html
+                  Medical Examination
                 </Link>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/immigration-refugees-citizenship/services/application/medical-police/medical-exams.html"
+                    'https://www.canada.ca/en/immigration-refugees-citizenship/services/visit- canada/parent-grandparent-super-visa/eligibility.html'
                   }
                 >
-                  https://www.canada.ca/en/immigration-refugees-citizenship/services/application/medical-police/medical-exams.html
+                  Eligibility for Super visa
                 </Link>
                 <Link
-                  className="text-sm text-white underline"
+                  className='text-sm text-white underline'
                   href={
-                    "https://www.canada.ca/en/revenue-agency/services/tax/international-non-residents/individuals-leaving-entering-canada-non-residents/newcomers-canada-immigrants.html"
+                    'https://www.canada.ca/en/immigration-refugees-citizenship/services/application/application-forms-guides/guide-5256-applying-visitor-visa-temporary-resident-visa.html#incometables'
                   }
                 >
-                  https://www.canada.ca/en/revenue-agency/services/tax/international-non-residents/individuals-leaving-entering-canada-non-residents/newcomers-canada-immigrants.html
+                  Income Requirements for super visa
+                </Link>
+                <Link
+                  href={
+                    'https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html'
+                  }
+                  className='text-sm text-white underline'
+                >
+                  Super visa processing time
                 </Link>
               </div>
             </div>
